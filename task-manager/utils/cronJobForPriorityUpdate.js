4353473f3c5faba3b12d4d4d9e44a7a8499b5730
SANTHOSH HV <116883776+hvsanthosh@users.noreply.github.com>
2024-01-28 02:46:10 +0530

    Update cronJobForPriorityUpdate.js
    modified functionality to update priority of the tasks based on the due date.

--- task-manager/utils/cronJobForPriorityUpdate.js
+++ task-manager/utils/cronJobForPriorityUpdate.js
@@ -6,93 +6,66 @@ import User from "../models/User.js";
 const cronJobForPRiorityUpdateInTask = () => {
   // console.log("cron logic...");
   // cron job is scheduled to run every minute "* * * * *"
+  
   cron.schedule("* * * * *", async () => {
-    console.log("Cron updating priority based on due_date..");
+    console.log("Cron updating priority based on due_date...");
 
     try {
-      // Get current date
+      // Get all tasks
+      // day zero
       const today = new Date();
-
-      // Find tasks with due_date today
-      const todayTasks = await Task.find({
-        due_date: {
-          $gte: today,
-          $lt: new Date(today.setDate(today.getDate() + 1)),
-        },
-      });
-      // console.log(`Today tasks ${todayTasks}`);
-
-      // Update priority based on due date
-      todayTasks.forEach(async (task) => {
-        // console.log(`Today task ${task}`);
-        task.priority = 0;
-        const user = await User.findById({ _id: task.createdBy });
-        user.priority = 0;
-        await user.save();
-        await task.save();
-      });
-
+      // day 1
       const tomorrow = new Date();
       tomorrow.setDate(tomorrow.getDate() + 1);
+      // day 2
+      const dayTwo = new Date();
+      dayTwo.setDate(dayTwo.getDate() + 2);
+      // day 3
+      const dayThree = new Date();
+      dayThree.setDate(dayThree.getDate() + 3);
+      // day 4
+      const dayFour = new Date();
+      dayFour.setDate(dayFour.getDate() + 4);
+      // day 5
+      const dayFive = new Date();
+      dayFive.setDate(dayFive.getDate() + 5);
 
-      // Find tasks with due_date between tomorrow and day after tomorrow
-      const tomorrowTasks = await Task.find({
-        due_date: {
-          $gte: tomorrow,
-          $lt: new Date(tomorrow.setDate(tomorrow.getDate() + 1)),
-        },
+      // get all tasks from collection
+      const allTasks = await Task.find();
+      // go trough all tasks and find its due_date and update priority accordingly
+      allTasks.forEach(async (task) => {
+        if (task.due_date >= today && task.due_date < tomorrow) {
+          task.priority = 0;
+          await task.save();
+        } else if (task.due_date >= tomorrow && task.due_date < dayTwo) {
+          task.priority = 1;
+
+          await task.save();
+        } else if (task.due_date >= dayTwo && task.due_date < dayThree) {
+          task.priority = 2;
+
+          await task.save();
+        } else if (task.due_date >= dayFive) {
+          task.priority = 3;
+
+          await task.save();
+        }
       });
 
-      // Update priority based on due date
-      tomorrowTasks.forEach(async (task) => {
-        // console.log(`Tomorrow task ${task}`);
+      // get all users
+      const allUsers = await User.find();
 
-        task.priority = 1;
-        const user = await User.findById({ _id: task.createdBy });
-        user.priority = 1;
+      // update priority for each user based on tasks
+      // assigning priority based on the tasks priority. min value.
+      allUsers.forEach(async (user) => {
+        // get tasks of user
+        const userTasks = await Task.find({ createdBy: user._id });
+
+        const taskPriorities = userTasks.map((task) => task.priority);
+        const minPriority = Math.min(...taskPriorities);
+        user.priority = minPriority;
+        // console.log(`${user.name} has priority ${user.priority}`);
         await user.save();
-        await task.save();
-      });
-
-      const threeDaysLater = new Date();
-      threeDaysLater.setDate(threeDaysLater.getDate() + 3);
-
-      // Find tasks with due_date between three and four days later
-      const nearFutureTasks = await Task.find({
-        due_date: {
-          $gte: threeDaysLater,
-          $lt: new Date(threeDaysLater.setDate(threeDaysLater.getDate() + 1)),
-        },
-      });
-
-      // Update priority based on due date
-      nearFutureTasks.forEach(async (task) => {
-        // console.log(`Further task ${task}`);
-
-        task.priority = 2;
-        const user = await User.findById({ _id: task.createdBy });
-        user.priority = 2;
-        await user.save();
-        await task.save();
-      });
-
-      const fiveDaysLater = new Date();
-      fiveDaysLater.setDate(fiveDaysLater.getDate() + 5);
-
-      // Find tasks with due_date after or in 5 days
-      const longTermTasks = await Task.find({
-        due_date: { $gte: fiveDaysLater },
-      });
-
-      // Update priority based on due date
-      longTermTasks.forEach(async (task) => {
-        // console.log(`Long term ${task}`);
-
-        task.priority = 3;
-        const user = await User.findById({ _id: task.createdBy });
-        user.priority = 3;
-        await user.save();
-        await task.save();
       });
 
       console.log("Priority updated successfully");
@@ -100,6 +73,7 @@ const cronJobForPRiorityUpdateInTask = () => {
       console.error("Error updating priority:", error);
     }
   });
+ 
 };
 
 export { cronJobForPRiorityUpdateInTask };
